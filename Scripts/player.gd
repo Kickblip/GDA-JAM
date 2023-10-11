93ba6feb1b876852b26355fd3b0f3d0d456915b8
chip03 <111972339+chip003@users.noreply.github.com>
2023-10-10 18:25:55 -0600

    Add rat transition animations and lighting system

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -6,26 +6,43 @@ var maxSpeed = 150 #maximum horizontal speed
 var gravity = 2 #rate at which player falls
 var terminalVelocity = 500 #maximum fall speed
 var dir = 1
+var lastHVel = 0
+var checkLanding = false
+var update = false
 
 @onready var sprite = $Sprite
 
-func _process(delta):
+func _ready():
+	sprite.play("idle_right")
+
+func _process(delta): 
 	var hVel = 0-int(Input.is_action_pressed("left"))+int(Input.is_action_pressed("right"))
 	
-	if hVel == -1:
-		dir = -1
-		sprite.play("move_left")
-		sprite.speed_scale = clamp(abs(velocity.x)/maxSpeed,0.5,1)
-	elif hVel == 1:
-		sprite.speed_scale = clamp(abs(velocity.x)/maxSpeed,0.5,1)
-		dir = 1
-		sprite.play("move_right")
-	else:
-		if dir == -1:
-			sprite.play("idle_left")
-		else:
-			sprite.play("idle_right")
+	#would like to convert a lot of these if statements into something better, but OK for now
+	if (hVel != lastHVel) || update: 
+		update = false
 		
+		if is_on_floor():
+			if hVel == -1: #moving left
+				dir = -1
+				sprite.play("transition_toMove_left")
+				#sprite.speed_scale = clamp(abs(velocity.x)/maxSpeed,0.5,1)
+			elif hVel == 1: #moving right
+				#sprite.speed_scale = clamp(abs(velocity.x)/maxSpeed,0.5,1)
+				dir = 1
+				sprite.play("transition_toMove_right")
+			else:
+				#sprite.speed_scale = 1
+				if dir == -1:
+					sprite.play("transition_toIdle_left")
+				else:
+					sprite.play("transition_toIdle_right")
+		else:
+			if hVel == -1:
+				sprite.play("air_left")
+			else:
+				sprite.play("air_right")
+		lastHVel = hVel
 	
 	if hVel != 0:
 		if abs(velocity.x) < maxSpeed:
@@ -39,4 +56,23 @@ func _process(delta):
 	if Input.is_action_just_pressed("jump") && is_on_floor():
 		velocity.y = -200
 		
+	if !is_on_floor():
+		if !checkLanding: #happens on jump
+			update = true
+			checkLanding = true
+	else: #player has landed
+		if checkLanding: #happens on land
+			checkLanding = false
+			update = true
+		
 	move_and_slide()
+
+func _on_sprite_animation_finished():
+	if sprite.animation == "transition_toIdle_right":
+		sprite.play("idle_right")
+	elif sprite.animation == "transition_toIdle_left":
+		sprite.play("idle_left")
+	elif sprite.animation == "transition_toMove_right":
+		sprite.play("move_right")
+	elif sprite.animation == "transition_toMove_left":
+		sprite.play("move_left")

88a5571b51cb84b345da55a81b9f76fab9f8ec5b
19tylermalone94 <19tylermalone94@gmail.com>
2023-10-21 23:14:19 -0600

    rocket collision and desconstruction

--- Objects/fireball.gd
+++ Objects/fireball.gd
@@ -4,12 +4,15 @@ var speed = 1
 var direction = 1
 var sprite_direction
 var colors = ["red", "white", "blue"]
+var origin
+var max_dist = 1000
 
 @onready var sprite = $Sprite
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	origin = position
 	randomize()
 	sprite_direction = "_right" if direction == 1 else "_left"
 	var color_index = randi() % len(colors)
@@ -18,5 +21,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if abs(origin.x - position.x) > max_dist:
+		queue_free()
 	move_local_x(speed * direction)
+	var collision_info = move_and_collide(Vector2(speed * direction, 0))
+	if collision_info:
+		queue_free()
 	

10a96c2882d24d636ca16c81345374e36dc6db0d
19tylermalone94 <19tylermalone94@gmail.com>
2023-10-25 00:00:06 -0600

    big flea enemy

---
+++ Objects/flea_enemy.gd
@@ -0,0 +1,58 @@
+extends CharacterBody2D
+
+var gravity = 480
+var init_pos
+var acceleration = 5
+var pursuit_speed = 100
+var idle_speed = 20
+var max_speed = idle_speed
+var jump_height = 100  # you can adjust this value
+var aware_radius = 200 # max distance to notice player
+var direction = 1
+var sprite_direction = "_right"
+var action = "idle"
+var damage = 20
+
+var player
+
+@onready var sprite = $Sprite
+@onready var jump_timer = $Timer
+
+
+func _ready():
+	sprite.play("fly_right")
+	velocity.x += acceleration
+	init_pos = position
+	jump_timer.wait_time = 2
+
+
+func _process(delta):
+	var players = get_tree().get_nodes_in_group("Player")
+	if players.size() > 0:
+		player = players[0]
+
+		if position.distance_to(player.global_position) < aware_radius:
+			max_speed = pursuit_speed
+			pursue_player()
+
+	if not is_on_floor():
+		action = "jump"
+		velocity.y += gravity * delta
+
+	move_and_slide()
+	sprite_direction = "_right" if direction == 1 else "_left"
+	sprite.play(action + sprite_direction)
+
+
+func pursue_player():
+	var player_hdist = position.x - player.global_position.x
+	direction = 1 if player_hdist < 0 else -1
+
+	#jump
+	if is_on_floor():
+		action = "idle"
+		if jump_timer.time_left < 0.05:
+			velocity.y = -sqrt(2 * gravity * jump_height)
+			jump_timer.start()
+		velocity.x = direction * pursuit_speed
+
